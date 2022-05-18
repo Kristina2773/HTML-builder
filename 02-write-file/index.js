f6a0f226ff4023aca0980c8c93b14df420117272
Kristina <d.kristina27@yandex.ru>
2022-05-18 22:36:03 +0400

    feat: complete second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const info = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const rl = readline.createInterface(stdin, stdout);
+
+rl.write('Hello, what are you tell me?\n')
+rl.on('line', (input) => {
+    if (input === 'exit') {
+        stdout.write('Bye\n');
+        process.exit();
+    }
+    info.write(`${input}\n`);
+});
+
+rl.on('close', () => {
+    stdout.write('Bye\n');
+    process.exit();
+});
