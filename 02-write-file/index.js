6aa4d7d9ef39e5073cbb2307f4906dc655fb97d2
Kristina <d.kristina27@yandex.ru>
2022-05-23 21:19:33 +0400

    fix: delete spaces in second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,13 +8,13 @@ const readline = require('readline');
 const info = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const rl = readline.createInterface(stdin, stdout);
 
-rl.write('Hello, what are you tell me?\n')
+rl.write('Hello, what are you tell me?\n');
 rl.on('line', (input) => {
-    if (input === 'exit') {
-        stdout.write('Bye\n');
-        process.exit();
-    }
-    info.write(`${input}\n`);
+  if (input === 'exit') {
+    stdout.write('Bye\n');
+    process.exit();
+  }
+  info.write(`${input}\n`);
 });
 
 rl.on('close', () => {
